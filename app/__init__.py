70719c6722f7276a08c371b246f53157b757ac66
Kiptim <kiptim54@gmail.com>
2018-04-15 12:50:28 +0300

    database reads login

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 
@@ -19,6 +22,7 @@ def create_app(config_name):
     #initialize flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
   # Registering the blueprint
     from .main import main as main_blueprint
@@ -27,8 +31,6 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
-    login_manager = LoginManager()
-    login_manager.session_protection= 'strong'
-    login_manager.login_view = 'auth.login'
+  
 
     return app
--- app/auth/views.py
+++ app/auth/views.py
@@ -16,7 +16,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
@@ -34,6 +34,6 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+    title = "New Account"
+    return render_template('auth/register.html',registration_form = form, title=title)
 
