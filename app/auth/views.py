129612de937dd8717f9a9fb9db6cfeb7a1caee27
Kiptim <kiptim54@gmail.com>
2018-04-15 18:22:57 +0300

    login and logout added

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,19 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required, current_user
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
+from flask_login import UserMixin
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+
     login_form = LoginForm()
+    if current_user.is_authenticated:
+        flash('Already logged in')
+        return redirect(url_for('main.index'))
+        
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
@@ -15,7 +21,6 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or Password')
-
     title = "login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
